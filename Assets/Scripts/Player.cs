8ef57ada79141947068cb9f412fc86ae62c21164
Presunto Parma <arthurgobbetti@gmail.com>
2025-11-27 15:03:17 -0300

    Implemented camre follow and jump animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
@@ -8,6 +9,8 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+
+    [Header("Movement")]
     public Vector2 friction = new Vector2(0.1f, 0f);
 
     public float speed;
@@ -15,6 +18,12 @@ public class Player : MonoBehaviour
     public float jumpForce;
 
     private float currentSpeed;
+
+    [Header("Animation")]
+    public float animationDuration = 0.3f;
+    private float jumpScaleY = 1.5f;
+    private float jumpScaleX = 0.7f;
+    public Ease ease = Ease.OutBack;
 
     void Update()
     {
@@ -57,6 +66,15 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector2.up * jumpForce;
+            rb.transform.localScale = Vector2.one;
+            DOTween.Kill(rb.transform);
+            ScaleJump();
         }
+    }
+
+    private void ScaleJump()
+    {
+        rb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
