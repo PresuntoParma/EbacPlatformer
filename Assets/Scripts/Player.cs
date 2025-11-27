e828e85f6fc19f556fcac7701d1449677d9a71ca
Presunto Parma <arthurgobbetti@gmail.com>
2025-11-27 14:27:47 -0300

    Implemented Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(0.1f, 0f);
 
     public float speed;
+    public float jumpForce;
 
     void Update()
+    {
+        Jump();
+        Walk();
+    }
+
+    private void Walk()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -19,6 +28,23 @@ public class Player : MonoBehaviour
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+        if (rb.velocity.x > 0)
+        {
+            rb.velocity -= friction;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity += friction;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
         }
     }
 }
