027a7c584b40a50d99611b50859109e9941a9d49
Presunto Parma <arthurgobbetti@gmail.com>
2026-02-09 21:06:28 -0300

    Added two more collectables

---
+++ Assets/Scripts/ItemCollectableBigCoin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectableBigCoin : ItemCollectableBase
+{
+    protected override void OnCollect()
+    {
+        base.OnCollect();
+        ItemManager.Instance.AddCoins(5);
+    }
+
+}
